b5bc0b85643cdc0183009e0ca79e14c35b0e8a6c
Erisa <101444750+Asayukiii@users.noreply.github.com>
2023-11-12 16:44:13 -0600

    Delete playground directory

--- playground/condition.js
+++
@@ -1,5 +0,0 @@
-const { Condition } = require('../dist/core/Condition.js')
-const x = require('../dist/main')
-
-const condition = new Condition
-const res = condition.resolve('1 > 0 && 5 < 10 && (12 > 6 || 13 < 1)')
