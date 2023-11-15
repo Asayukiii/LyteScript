df1dc2d0be02f7bd0eca69068b719f026bd6c93a
Asayukiii <alphadlpop6@gmail.com>
2023-11-15 08:50:10 -0600

    added @replaceLast

---
+++ src/functions/replaceLast.ts
@@ -0,0 +1,36 @@
+import { NativeFunction, ParameterType } from '../main.js'
+
+export default new NativeFunction({
+    name: '@replaceLast',
+    description: 'Replaces all matches in a string.',
+    parameters: [
+        {
+            name: 'Text',
+            description: 'The text to work with.',
+            type: ParameterType.String,
+            required: true
+        },
+        {
+            name: 'Match',
+            description: 'The thing to match in the string.',
+            type: ParameterType.String,
+            required: true
+        },
+        {
+            name: 'Replacement',
+            description: 'The replacement.',
+            type: ParameterType.String,
+            required: true
+        }
+    ],
+    execute: async function (d) {
+        const [text, match, replacer] = d.function!.compiled.parameters.map(t => t.value)
+        const args = text.split(' ')
+        const found = [...args].reverse().find(split => split.includes(split))
+        const idx = args.lastIndexOf(found ?? '')
+        if (idx !== -1) {
+            args[idx] = args[idx]?.replace(new RegExp(`${match}`), replacer)
+        }
+        return args.join(' ')
+    }
+})
